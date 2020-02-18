f6fc7f4234bd78b519e11ac474ca8b409fa54bed
lukasyano <toliusislukas@yahoo.com>
2020-02-18 20:46:44 +0200

    Built movie details models

---
+++ app/src/main/java/com/lukas/awesomemovies/data/network/model/ProductionCompany.kt
@@ -0,0 +1,8 @@
+package com.lukas.awesomemovies.data.network.model
+
+data class ProductionCompany(
+    val id: Int,
+    val logo_path: String,
+    val name: String,
+    val origin_country: String
+)
