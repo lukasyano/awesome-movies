93ed1b6d3ef8025dedeb5b9d127a06cc6eb4e49d
lukasyano <toliusislukas@yahoo.com>
2020-02-20 20:44:22 +0200

    added paper library

--- app/src/main/java/com/lukas/awesomemovies/AMoviesApplication.kt
+++ app/src/main/java/com/lukas/awesomemovies/AMoviesApplication.kt
@@ -3,6 +3,8 @@ package com.lukas.awesomemovies
 import android.app.Application
 import com.lukas.awesomemovies.data.network.MoviesService
 import com.lukas.awesomemovies.data.network.NetworkServiceGenerator
+import io.paperdb.Paper
+import timber.log.Timber
 
 class AMoviesApplication : Application() {
 
@@ -11,5 +13,11 @@ class AMoviesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         movieService = NetworkServiceGenerator().retrofit.create(MoviesService::class.java)
+
+        Paper.init(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
